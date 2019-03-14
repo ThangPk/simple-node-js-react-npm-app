a11ed5af346ffee1598a998c2b973e0f3684e74d
Thang <thangdx2901@gmail.com>
2019-03-15 00:23:51 +0700

    Again :)

--- src/tools/api-dev-install.sh
+++ src/tools/api-dev-install.sh
@@ -1,10 +1,10 @@
 
 
 echo '>>>>> Installing the requirements for servers..........'
-sudo -H pip3 install -r /src/server/requirements.txt
+sudo -H pip3 install -r ./src/server/requirements.txt
 
 echo '>>>>> Compiling the python server sources............'
-cd /src/tools
+cd ./src/tools
 
 echo ">>>>> Compile common package"
 touch .module_common
@@ -17,14 +17,14 @@ python3 build.py build_ext
 rm .module_api_server
 
 echo '>>>>> Change permission to build folders..........'
-sudo chmod -R 777 $./src/tools/dist
+sudo chmod -R 777 ./src/tools/dist
 
 echo '>>>>> Migrating database..........'
-cd /src/tools/dist/common/db
+cd ./src/tools/dist/common/db
 orator migrate
 
 echo '>>>>> Coping license files..........'
-cp -a /backup/server/license/. /src/tools/dist/api-server/
+cp -a ./backup/server/license/. ./src/tools/dist/api-server/
 
 echo '>>>>> Restarting servers..........'
 sudo systemctl restart vms-api-dev-server
